4109c9b87f069b2e22756c1e4ddb61bbdb11e427
Amal <61520524+AmalAljabri@users.noreply.github.com>
2021-07-24 10:37:13 +0300

    Update serve-slides.sh

--- serve-slides.sh
+++ serve-slides.sh
@@ -1,1 +1,1 @@
-jupyter nbconvert Ford_GoBike_System_Data_Slide_Deck.ipynb --to slides --post serve --template output_toggle
+jupyter nbconvert Ford_GoBike_System_Data_Slide_Deck.ipynb --to slides --TemplateExporter.exclude_input=True --post serve
